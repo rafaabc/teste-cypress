5ab9773e33a5a6d77dd2f8f9124bdab4e962c804
rafaabc <faelsabc21@gmail.com>
2021-02-01 09:23:31 -0300

    refactoring of functional scenarios

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -9,6 +9,11 @@ describe('Should test at a functional level', () => {
     cy.resetApp()
   })
 
+  beforeEach(() => {
+    cy.get(loc.MENU.HOME).click()
+    cy.resetApp()
+  })
+
   it('Should insert an account', () => {
     cy.acessarMenuConta()
     cy.inserirConta('Conta de teste')
@@ -17,7 +22,7 @@ describe('Should test at a functional level', () => {
 
   it('Should edit an account', () => {
     cy.acessarMenuConta()
-    cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta de teste')).click()
+    cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click()
     cy.get(loc.CONTAS.NOME).clear().type('Conta alterada')
     cy.get(loc.CONTAS.BTN_SALVAR).click()
     cy.get(loc.MESSAGE).should('contain', 'atualizada com sucesso')
@@ -25,7 +30,7 @@ describe('Should test at a functional level', () => {
 
   it('Should not create an account with the same name', () => {
     cy.acessarMenuConta()
-    cy.get(loc.CONTAS.NOME).type('Conta alterada')
+    cy.get(loc.CONTAS.NOME).type('Conta mesmo nome')
     cy.get(loc.CONTAS.BTN_SALVAR).click()
     cy.get(loc.MESSAGE).should('contain', 'code 400')
   })
@@ -35,7 +40,7 @@ describe('Should test at a functional level', () => {
     cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
     cy.get(loc.MOVIMENTACAO.VALOR).type('100')
     cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
-    cy.get(loc.MOVIMENTACAO.CONTA).select('Conta alterada')
+    cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
     cy.get(loc.MOVIMENTACAO.STATUS).click()
     cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
     cy.get(loc.MESSAGE).should('contain', 'sucesso')
@@ -46,12 +51,20 @@ describe('Should test at a functional level', () => {
 
   it('Should get balance', () => {
     cy.get(loc.MENU.HOME).click()
-    cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta alterada')).should('contain', '100,00')
+    cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+    cy.get(loc.MENU.EXTRATO).click()
+    cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
+    cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
+    cy.get(loc.MOVIMENTACAO.STATUS).click()
+    cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+    cy.get(loc.MESSAGE).should('contain', 'Movimentação alterada com sucesso')
+    cy.get(loc.MENU.HOME).click()
+    cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
   })
 
   it('Should remove a transaction', () => {
     cy.get(loc.MENU.EXTRATO).click()
-    cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Desc')).click()
+    cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
     cy.get(loc.MESSAGE).should('contain', 'removida com sucesso')
   })
 })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -28,7 +28,8 @@ const locators = {
   EXTRATO: {
     LINHAS: '.list-group > li',
     FN_XP_BUSCA_ELEMENTO: (desc, value) => `//span[contains(., '${desc}')]/following-sibling::small[contains(., '${value}')]`,
-    FN_XP_REMOVER_ELEMENTO: conta => `//span[contains(., '${conta}')]/..//..//..//i[@class='far fa-trash-alt']`
+    FN_XP_REMOVER_ELEMENTO: conta => `//span[contains(., '${conta}')]/..//..//..//i[@class='far fa-trash-alt']`,
+    FN_XP_ALTERAR_ELEMENTO: conta => `//span[contains(., '${conta}')]/..//..//..//i[@class='fas fa-edit']`
   },
   SALDO: {
     FN_XP_SALDO_CONTA: nome => `//td[contains(., '${nome}')]/../td[2]`
